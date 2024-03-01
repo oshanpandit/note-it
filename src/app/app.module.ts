43b705fb730d17b42cb60c3244aef91a014edb43
oshan pandit <oshanpandit@gmail.com>
2024-03-01 19:24:31 +0530

    masonry implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import { NotesComponent } from './notes/notes.component';
 import { ArchiveComponent } from './archive/archive.component';
 import { TrashComponent } from './trash/trash.component';
 import { FilterPipePipe } from './filter-pipe.pipe';
+import { TestComponent } from './test/test.component';
 
 
 const appRoutes:Routes=[
   {path:'',component:NotesComponent},
   {path:'archive',component:ArchiveComponent},
-  {path:'trash',component:TrashComponent}
+  {path:'trash',component:TrashComponent},
+  {path:'test',component:TestComponent}
 ];
 
 @NgModule({
@@ -26,7 +28,8 @@ const appRoutes:Routes=[
     NotesComponent,
     ArchiveComponent,
     TrashComponent,
-    FilterPipePipe
+    FilterPipePipe,
+    TestComponent
   ],
   imports: [
     BrowserModule,
--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DataService } from '../data.service';
 import { OnInit } from '@angular/core';
+import { AfterViewInit,ElementRef,ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-notes',
@@ -10,12 +11,13 @@ import { OnInit } from '@angular/core';
 export class NotesComponent {
 
     constructor(private dataService:DataService){};
+    @ViewChild('container') container: ElementRef;
     noteItems:{title:string,content:string}[];
     curr_title="";
     curr_content="";
-
     curr_search='';
-
+    display=600;
+    
     ngOnInit(){
       this.noteItems=this.dataService.noteitems;
       this.dataService.sharedString$.subscribe(newString => {
@@ -24,16 +26,23 @@ export class NotesComponent {
     }
 
     addTask(){
+        this.display+=this.curr_content.length/2.2;
+        this.container.nativeElement.style.height=this.display.toString()+'px';
        if(this.curr_content!=''){
           this.dataService.addItem(this.curr_title,this.curr_content);
        }
     }
     removeTask(index:number){
-       this.dataService.removeItem(index);
+       
+        let diff=this.noteItems[index].content.length/2;
+        this.display=this.display-diff;
+        this.container.nativeElement.style.height=this.display.toString()+'px';
+        this.dataService.removeItem(index);
     }
     
     archiveTask(index:number){
        this.dataService.archiveTask(index);
     }
+
 
 }
